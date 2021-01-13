228c5c61bbb8d7b4a52e29484a73dd4621f0e07e
Daniel Cho <danielcho91@gmail.com>
2021-01-13 18:23:52 -0500

    updated data to have images

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,75 +1,100 @@
+import cake from './images/cake.jpg';
+import canadagoosebomber from './images/canadagoosebomber.jpg';
+import cola from './images/cola.jpg';
+import croissant from './images/croissant.jpg';
+import fenderguitar from './images/fenderguitar.jpg';
+import galaxynote10 from './images/galaxynote10.jpg';
+import kiwi from './images/kiwi.jpg';
+import martinguitar from './images/martinguitar.jpg';
+import milk from './images/milk.jpg';
+import nikeaf1 from './images/nikeaf1.jpg';
+import samsungtv from './images/samsungtv.png';
+import strawberry from './images/strawberry.jpg';
+
 const productData = [
   {
     name: 'Cake',
     category: 'bakery',
     price: 19.99,
-    quantity: 5
+    quantity: 0,
+    image: cake
   },
   {
     name: 'Croissant',
     category: 'bakery',
     price: 12.99,
-    quantity: 5
+    quantity: 1,
+    image: croissant
   },
   {
     name: 'Kiwi 20 lbs',
     category: 'grocery',
     price: 39.99,
-    quantity: 5
+    quantity: 5,
+    image: kiwi
   },
   {
     name: 'strawberry 10 lbs',
     category: 'grocery',
     price: 29.99,
-    quantity: 5
+    quantity: 5,
+    image: strawberry
   },
   {
     name: 'Martin Standard Series D-35',
     category: 'instrument',
     price: 999.99,
-    quantity: 5
+    quantity: 5,
+    image: martinguitar
   },
   {
     name: 'Fender Stratocaster',
     category: 'instrument',
     price: 1299.99,
-    quantity: 5
+    quantity: 5,
+    image: fenderguitar
   },
   {
     name: 'Canada Goose - Bomber',
     category: 'clothes',
     price: 799.99,
-    quantity: 5
+    quantity: 5,
+    image: canadagoosebomber
   },
   {
     name: 'Nike - Air Force 1',
     category: 'clothes',
     price: 129.99,
-    quantity: 5
+    quantity: 5,
+    image: nikeaf1
   },
   {
     name: 'Coca Cola Bottle',
     category: 'convenience',
     price: 2.49,
-    quantity: 5
+    quantity: 5,
+    image: cola
   },
   {
     name: 'Milk 2L',
     category: 'convenience',
     price: 3.49,
-    quantity: 5
+    quantity: 5,
+    image: milk
   },
   {
     name: 'Galaxy Note 10',
     category: 'samsung',
     price: 1234.99,
-    quantity: 5
+    quantity: 5,
+    image: galaxynote10
   },
   {
     name: 'Samsung Curved TV 65 inch',
     category: 'samsung',
     price: 699.99,
-    quantity: 5
+    quantity: 5,
+    image: samsungtv
   }
 ];
 
