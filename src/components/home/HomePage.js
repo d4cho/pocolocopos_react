325012b5166475ad1ca938ce92b271a25ed7b0c9
Daniel Cho <danielcho91@gmail.com>
2021-01-14 18:55:49 -0500

    added icons and fixed css on homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,39 +1,56 @@
 import React from 'react';
 import './HomePage.css';
 import homeImg from '../../assets/images/open.jpg';
+import SettingsIcon from '@material-ui/icons/Settings';
+import HelpIcon from '@material-ui/icons/Help';
+import ApartmentIcon from '@material-ui/icons/Apartment';
+import BarChartIcon from '@material-ui/icons/BarChart';
+import BallotIcon from '@material-ui/icons/Ballot';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 const HomePage = (props) => {
   return (
-    <div class='home-container'>
+    <div className='home-container'>
       <img src={homeImg} alt={'home'} />
       <div className='home-menu-container'>
-        <div style={{ fontSize: '4vw' }}>
+        <div style={{ fontSize: '3.5vw' }}>
           <strong>POCOLOCO</strong>
         </div>
-        <div style={{ fontSize: '1.8vw' }}>Guest Testing</div>
+        <div style={{ fontSize: '2vw' }}>Guest Testing</div>
         <div className='home-menu-controls-container'>
           <button
             style={{ cursor: 'pointer' }}
             onClick={() => props.refreshFunction(2)}>
-            CASH REGISTER
+            <MonetizationOnIcon fontSize='inherit' htmlColor='gold' />
+            <div style={{ fontSize: '1vw' }}>CASH REGISTER</div>
           </button>
           <button disabled style={{ cursor: 'not-allowed' }}>
-            INVENTORY
+            <BallotIcon fontSize='inherit' htmlColor='purple' />
+            <div style={{ fontSize: '1vw' }}>INVENTORY</div>
           </button>
           <button disabled style={{ cursor: 'not-allowed' }}>
-            REPORT
+            <BarChartIcon fontSize='inherit' htmlColor='blue' />
+            <div style={{ fontSize: '1vw' }}>REPORT</div>
           </button>
           <button disabled style={{ cursor: 'not-allowed' }}>
-            ARIES
+            <ApartmentIcon fontSize='inherit' htmlColor='navy' />
+            <div style={{ fontSize: '1vw' }}>ARIES</div>
           </button>
           <button disabled style={{ cursor: 'not-allowed' }}>
-            HELP
+            <HelpIcon fontSize='inherit' htmlColor='orange' />
+            <div style={{ fontSize: '1vw' }}>HELP</div>
           </button>
           <button disabled style={{ cursor: 'not-allowed' }}>
-            SETTING
+            <SettingsIcon fontSize='inherit' htmlColor='teal' />
+            <div style={{ fontSize: '1vw' }}>SETTING</div>
           </button>
         </div>
-        <div style={{ fontSize: '1.5vw' }}>Logged in as GUEST</div>
+        <div
+          style={{ display: 'flex', alignItems: 'center', fontSize: '1.2vw' }}>
+          <FiberManualRecordIcon htmlColor='lime' />
+          &nbsp; Logged in as GUEST
+        </div>
         <button
           disabled
           style={{
@@ -41,7 +58,7 @@ const HomePage = (props) => {
             padding: '0',
             border: 'none',
             background: 'none',
-            fontSize: '1.5vw'
+            fontSize: '1.3vw'
           }}>
           [&nbsp;&nbsp;LOGOUT&nbsp;&nbsp;]
         </button>
