0790cf981ba5acd782cdc8f2572c7db09dc18fa8
Daniel Cho <danielcho91@gmail.com>
2021-01-14 18:56:49 -0500

    added icons and fixed css for sidebar

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Sidebar.css';
+import HomeIcon from '@material-ui/icons/Home';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import ReceiptIcon from '@material-ui/icons/Receipt';
 
 const Sidebar = (props) => {
   // 1 = home, 2 = cashier, 3 = invoice
@@ -15,27 +18,44 @@ const Sidebar = (props) => {
     props.refreshFunction(parseInt(e.target.id));
   };
 
+  const divStyle = {
+    selected: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-evenly',
+      backgroundColor: 'orange'
+    },
+    notSelected: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-evenly'
+    }
+  };
+
   return (
     <div className='sidebar-grid-container'>
       <div
         id='1'
         className='sidebar-grid-item grid-item-home'
-        style={menuClicked === 1 ? { backgroundColor: 'orange' } : null}
+        style={menuClicked === 1 ? divStyle.selected : divStyle.notSelected}
         onClick={menuClickedHandler}>
+        <HomeIcon />
         HOME
       </div>
       <div
         id='2'
         className='sidebar-grid-item grid-item'
-        style={menuClicked === 2 ? { backgroundColor: 'orange' } : null}
+        style={menuClicked === 2 ? divStyle.selected : divStyle.notSelected}
         onClick={menuClickedHandler}>
+        <MonetizationOnIcon />
         CASHIER
       </div>
       <div
         id='3'
         className='sidebar-grid-item grid-item'
-        style={menuClicked === 3 ? { backgroundColor: 'orange' } : null}
+        style={menuClicked === 3 ? divStyle.selected : divStyle.notSelected}
         onClick={menuClickedHandler}>
+        <ReceiptIcon />
         INVOICE
       </div>
       <div className='sidebar-grid-item'></div>
