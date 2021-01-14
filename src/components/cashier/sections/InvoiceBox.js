7783cdc621684b26033c68ed8401efe91f1917bd
Daniel Cho <danielcho91@gmail.com>
2021-01-14 18:52:43 -0500

    invoice box icons

--- src/components/cashier/sections/InvoiceBox.js
+++ src/components/cashier/sections/InvoiceBox.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './InvoiceBox.css';
+import {
+  useProductList,
+  useProductListClear
+} from '../../../context/ProductContext';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import InvoiceBoxItem from './InvoiceBoxItem';
 
 const InvoiceBox = () => {
+  const clearProductList = useProductListClear();
+  const productList = useProductList();
+
   return (
     <div className='grid-container-invoice'>
       <div className='invoice-topbar'>
         <div>INVOICE #15</div>
-        <div>CLEAR CART</div>
+        <div
+          style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+          onClick={clearProductList}>
+          <DeleteForeverIcon />
+          CLEAR CART
+        </div>
         <div>GROSS DISCOUNT</div>
       </div>
       <div className='invoice-midbar'>
@@ -15,10 +29,14 @@ const InvoiceBox = () => {
         <div>Qty</div>
         <div>Price</div>
       </div>
-      <div className='invoice-body-empty'>
-        <div>icon</div>
-        <div>Add Product To Cart.</div>
-      </div>
+      {productList.length === 0 ? (
+        <div className='invoice-body-empty'>
+          <div>icon</div>
+          <div>Add Product To Cart.</div>
+        </div>
+      ) : (
+        <InvoiceBoxItem />
+      )}
     </div>
   );
 };
