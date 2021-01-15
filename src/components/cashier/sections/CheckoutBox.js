40ac845d99d12a90cd7237843dfa46022849c9d9
Daniel Cho <danielcho91@gmail.com>
2021-01-15 18:09:34 -0500

    checkout box with values

--- src/components/cashier/sections/CheckoutBox.js
+++ src/components/cashier/sections/CheckoutBox.js
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './CheckoutBox.css';
+import { useProductList } from '../../../context/ProductContext';
 
 const CheckoutBox = () => {
+  const [subtotal, setSubtotal] = useState(0);
+  const [tax, setTax] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const productList = useProductList();
+
+  useEffect(() => {
+    if (productList.length > 0) {
+      // calculate subtotal value
+      const subtotalValue = productList
+        .map((item) => item.productPrice)
+        .reduce((acc, currVal) => acc + currVal);
+      setSubtotal(subtotalValue);
+
+      // calculate tax value
+      const taxValue = subtotalValue * 0.05;
+      setTax(taxValue);
+
+      // calculate total value
+      const totalValue = subtotalValue + taxValue;
+      setTotal(totalValue);
+    } else {
+      setSubtotal(0);
+      setTax(0);
+      setTotal(0);
+    }
+  }, [productList]);
+
   return (
     <div className='checkout-container'>
       <div className='checkout-total-container'>
         <div className='subtotal-item'>Sub Total</div>
-        <div className='subtotal-value-item'>$0.00</div>
+        <div className='subtotal-value-item'>${subtotal.toFixed(2)}</div>
         <div className='tax-item'>Tax (5 Tax Rates)</div>
-        <div className='tax-value-item'>$0.00</div>
+        <div className='tax-value-item'>${tax.toFixed(2)}</div>
         <div className='coupon-item'>Coupon</div>
         <div className='coupon-value-item'>$0.00</div>
         <div className='hor-underline'></div>
@@ -16,7 +45,7 @@ const CheckoutBox = () => {
           <strong>Total</strong>
         </div>
         <div className='total-value-item'>
-          <strong>$0.00</strong>
+          <strong>${total.toFixed(2)}</strong>
         </div>
       </div>
       <button className='checkout-btn'>CHECKOUT</button>
