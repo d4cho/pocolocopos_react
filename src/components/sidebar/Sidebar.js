14169c14957a382c2f08c5e046ce5a610fe833ac
Daniel Cho <danielcho91@gmail.com>
2021-02-08 15:56:14 -0500

    sidebar icons

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -3,9 +3,11 @@ import './Sidebar.css';
 import HomeIcon from '@material-ui/icons/Home';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
 import ReceiptIcon from '@material-ui/icons/Receipt';
+import PersonIcon from '@material-ui/icons/Person';
+import LocalAtmIcon from '@material-ui/icons/LocalAtm';
 
 const Sidebar = (props) => {
-  // 1 = home, 2 = cashier, 3 = invoice
+  // 1 = home, 2 = cashier, 3 = invoice, 4 = account, 5 = special
   const [menuClicked, setMenuClicked] = useState(props.displayPage);
 
   useEffect(() => {
@@ -34,30 +36,41 @@ const Sidebar = (props) => {
   return (
     <div className='sidebar-grid-container'>
       <div
-        id='1'
-        className='sidebar-grid-item grid-item-home'
+        className='grid-item-home'
         style={menuClicked === 1 ? divStyle.selected : divStyle.notSelected}
         onClick={() => menuClickedHandler(1)}>
         <HomeIcon fontSize='large' />
         HOME
       </div>
       <div
-        id='2'
-        className='sidebar-grid-item grid-item'
+        className='grid-item'
         style={menuClicked === 2 ? divStyle.selected : divStyle.notSelected}
         onClick={() => menuClickedHandler(2)}>
         <MonetizationOnIcon fontSize='large' />
         CASHIER
       </div>
       <div
-        id='3'
-        className='sidebar-grid-item grid-item'
+        className='grid-item'
         style={menuClicked === 3 ? divStyle.selected : divStyle.notSelected}
         onClick={() => menuClickedHandler(3)}>
         <ReceiptIcon fontSize='large' />
         INVOICE
       </div>
-      <div className='sidebar-grid-item'></div>
+      <div
+        className='grid-item'
+        style={menuClicked === 4 ? divStyle.selected : divStyle.notSelected}
+        onClick={() => menuClickedHandler(4)}>
+        <PersonIcon fontSize='large' />
+        ACCOUNT
+      </div>
+      <div
+        className='grid-item'
+        style={menuClicked === 5 ? divStyle.selected : divStyle.notSelected}
+        onClick={() => menuClickedHandler(5)}>
+        <LocalAtmIcon fontSize='large' />
+        SPECIAL
+      </div>
+      <div></div>
     </div>
   );
 };
