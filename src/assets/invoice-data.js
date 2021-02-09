dd21dd705ad14e477c74d0f923f2bff886feb2e3
Daniel Cho <danielcho91@gmail.com>
2021-02-09 17:48:05 -0500

    change invoice data

--- src/assets/invoice-data.js
+++ src/assets/invoice-data.js
@@ -2,34 +2,29 @@ export const invoiceData = [
   {
     invoice: 1,
     date: '01/16/21',
-    time: '03:01 AM',
-    totalPrice: 678,
-    products: [
-      { name: 'Example Item 1', price: 100 },
-      { name: 'Example Item 2', price: 200 },
-      { name: 'Example Item 3', price: 300 }
-    ],
+    time: '03:00 PM',
+    totalPrice: 123,
+    products: [{ name: 'Example Item 1', price: 100 }],
     paymentMethod: 'cash',
     coupon: false
   },
   {
     invoice: 2,
-    date: '01/16/21',
-    time: '03:01 AM',
-    totalPrice: 678,
+    date: '01/17/21',
+    time: '03:15 PM',
+    totalPrice: 234,
     products: [
       { name: 'Example Item 1', price: 100 },
-      { name: 'Example Item 2', price: 200 },
-      { name: 'Example Item 3', price: 300 }
+      { name: 'Example Item 2', price: 200 }
     ],
     paymentMethod: 'cash',
     coupon: false
   },
   {
     invoice: 3,
-    date: '01/16/21',
-    time: '03:01 AM',
-    totalPrice: 678,
+    date: '01/18/21',
+    time: '03:30 PM',
+    totalPrice: 345,
     products: [
       { name: 'Example Item 1', price: 100 },
       { name: 'Example Item 2', price: 200 },
@@ -40,34 +35,37 @@ export const invoiceData = [
   },
   {
     invoice: 4,
-    date: '01/16/21',
-    time: '03:01 AM',
-    totalPrice: 678,
+    date: '01/19/21',
+    time: '03:45 PM',
+    totalPrice: 456,
     products: [
       { name: 'Example Item 1', price: 100 },
       { name: 'Example Item 2', price: 200 },
-      { name: 'Example Item 3', price: 300 }
+      { name: 'Example Item 3', price: 300 },
+      { name: 'Example Item 4', price: 400 }
     ],
     paymentMethod: 'cash',
     coupon: false
   },
   {
     invoice: 5,
-    date: '01/16/21',
-    time: '03:01 AM',
-    totalPrice: 678,
+    date: '01/20/21',
+    time: '04:00 PM',
+    totalPrice: 567,
     products: [
       { name: 'Example Item 1', price: 100 },
       { name: 'Example Item 2', price: 200 },
-      { name: 'Example Item 3', price: 300 }
+      { name: 'Example Item 3', price: 300 },
+      { name: 'Example Item 4', price: 400 },
+      { name: 'Example Item 5', price: 500 }
     ],
     paymentMethod: 'cash',
     coupon: false
   },
   {
     invoice: 6,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -79,8 +77,8 @@ export const invoiceData = [
   },
   {
     invoice: 7,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -92,8 +90,8 @@ export const invoiceData = [
   },
   {
     invoice: 8,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -105,8 +103,8 @@ export const invoiceData = [
   },
   {
     invoice: 9,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -118,8 +116,8 @@ export const invoiceData = [
   },
   {
     invoice: 10,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -131,8 +129,8 @@ export const invoiceData = [
   },
   {
     invoice: 11,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -144,8 +142,8 @@ export const invoiceData = [
   },
   {
     invoice: 12,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
@@ -157,8 +155,8 @@ export const invoiceData = [
   },
   {
     invoice: 13,
-    date: '01/16/21',
-    time: '03:01 AM',
+    date: '02/09/21',
+    time: '05:45 PM',
     totalPrice: 678,
     products: [
       { name: 'Example Item 1', price: 100 },
